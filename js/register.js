b130694e76b0cd45df63092851a738095021f879
Anirudh Is Online <59577127+anirudhisonline@users.noreply.github.com>
2023-03-05 09:56:06 +0530

    initial commit

---
+++ js/register.js
@@ -0,0 +1,14 @@
+sessionStorage.setItem("firstname", "");
+sessionStorage.setItem("lastname", "");
+sessionStorage.setItem("email", "");
+sessionStorage.setItem("mothername", "");
+sessionStorage.setItem("fathername", "");
+sessionStorage.setItem("phone", "");
+sessionStorage.setItem("age", "");
+sessionStorage.setItem("dob", "");
+sessionStorage.setItem("address", "");
+sessionStorage.setItem("pincode", "");
+sessionStorage.setItem("course", "");
+console.log("register");
+
+	
---
+++ php/register.php
@@ -0,0 +1,61 @@
+<?php 
+
+$db_user = "root";
+$db_pass = "";
+$db_name = "users";
+
+$db = new PDO('mysql:host=localhost;dbname=' . $db_name . ';charset=utf8', $db_user, $db_pass);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// $client = new MongoDB\Client('mongodb://localhost:27017');
+// $collection = (new MongoDB\Client)->users->profiles;
+
+
+
+
+
+
+if(isset($_POST)){
+
+	
+	$email 			= $_POST['email'];
+
+	
+	$password 		= sha1($_POST['password']);
+
+
+	// $firstname 			= $_POST['firstname'];
+	// $lastname 			= $_POST['lastname'];
+	// $mothername 			= $_POST['mothername'];
+	// $fathername 			= $_POST['fathername'];
+	// $address 			= $_POST['address'];
+	// $pincode 			= $_POST['pincode'];
+	// $dob 			= $_POST['dob'];
+	// $age 			= $_POST['age'];
+	// $course 			= $_POST['course'];
+
+		$sql = "INSERT INTO users (email, password ) VALUES(?,?)";
+		$stmtinsert = $db->prepare($sql);
+		$result = $stmtinsert->execute([$email, $password]);
+		if($result){
+			// $insertOneResult = $collection->insertOne([
+			// 	'firstname' => $firstname,
+			// 				'lastname' => $lastname,
+			// 				'mothername' => $mothername,
+			// 				'fathername' => $fathername,
+			// 				'address' => $address ,
+			// 				'pincode'=> $pincode,
+			// 				'dob'=> $dob ,
+			// 				'age' => $age,
+			// 				'course' => $course ,
+			// 				'email' => $email,
+			// ]);
+			
+			echo 'Successfully saved.';
+
+		}else{
+			echo 'There were erros while saving the data.';
+		}
+}else{
+	echo 'No data';
+}
